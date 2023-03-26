ed3a103c94d1ce04c23d34138a0659062d5805f2
iamchacha021 <iamchacha021@gmail.com>
2023-03-26 17:35:40 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, include: :items
+        else
+            render json: {error: "User does not exist"}, status: :not_found
+        end
+    end
 end
